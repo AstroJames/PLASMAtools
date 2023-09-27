4c0d231f3606a5230a3be91f1fa3e17fae4fc4af
AstroJames <james.beattie@princeton.edu>
2023-09-27 10:43:37 +0800

    Abstracting away some funcs.

--- read_flash.py
+++ read_flash.py
@@ -2,9 +2,9 @@ import time
 from uu import Error
 from h5py import File
 import numpy as np
-import numpy.fft as fft
 import numpy.polynomial.polynomial as poly
 import timeit
+import derived_var_funcs as dvf
 
 class Particles:
 
@@ -524,50 +524,13 @@ class Fields():
         if field_str == "helmholtz":
             self.read("vel")
             
-            F_irrot, F_solen = helmholtz_decomposition(np.array([self.velx, self.vely, self.velz]))
+            F_irrot, F_solen = dvf.helmholtz_decomposition(np.array([self.velx, self.vely, self.velz]))
             
             for coords, idx in enumerate(["x", "y", "z"]):
                 setattr(self, f"vel_comp{coords}", F_irrot[idx])
                 setattr(self, f"vel_sol{coords}", F_solen[idx])
                 
-            
-def helmholtz_decomposition(F,x):
-    # F is a 4D array, with the last dimension being 3 (for the x, y, z components of the vector field)
-    shape = F.shape[:-1]
-    Fhat = fft.fftn(F, axes=(0, 1, 2),norm = 'forward')
     
-    Fhat_irrot = np.zeros_like(Fhat, dtype=np.complex128)
-    Fhat_solen = np.zeros_like(Fhat, dtype=np.complex128)
-    norm       = np.zeros(shape, dtype=np.float64)
-    
-    # Compute wave numbers
-    kx = fft.fftfreq(shape[0])* 2*np.pi * shape[0] / (x[-1] - x[0])
-    ky = fft.fftfreq(shape[1])* 2*np.pi * shape[1] / (x[-1] - x[0])
-    kz = fft.fftfreq(shape[2])* 2*np.pi * shape[1] / (x[-1] - x[0])
-    KX, KY, KZ = np.meshgrid(kx, ky, kz, indexing='ij')
-    
-    # Avoid division by zero
-    norm = KX**2 + KY**2 + KZ**2
-    norm[0, 0, 0] = 1
-    
-    # Compute divergence and curl in Fourier space (note python doesn't seem to want to use i)
-    divFhat = (KX * Fhat[..., 0] + KY * Fhat[..., 1] + KZ * Fhat[..., 2])
-    
-    # Compute irrotational and solenoidal components in Fourier space
-    Fhat_irrot = np.transpose(divFhat * np.array([KX, KY, KZ]) / norm[np.newaxis, ...],(1,2,3,0))
-    Fhat_solen = Fhat - Fhat_irrot #curlFhat / norm[np.newaxis, ...]
-    
-    # Inverse Fourier transform to real space
-    F_irrot = fft.ifftn(Fhat_irrot, axes=(0, 1, 2)).real
-    F_solen = fft.ifftn(Fhat_solen, axes=(0, 1, 2)).real
-    
-    threshold = 1e-16
-    F_solen[np.abs(F_solen) < threshold] = 0
-    F_irrot[np.abs(F_irrot) < threshold] = 0
-    
-    return F_irrot, F_solen
-           
-                
 def reformat_FLASH_field(field  : np.ndarray,
                          nxb    : int,
                          nyb    : int,
