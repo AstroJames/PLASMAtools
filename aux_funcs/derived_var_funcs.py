09a54f886e98d82812952b13ba8db126a29a8362
AstroJames <james.beattie@princeton.edu>
2023-11-13 10:40:36 +0800

    Added Laplacian.

--- aux_funcs/derived_var_funcs.py
+++ aux_funcs/derived_var_funcs.py
@@ -22,11 +22,15 @@ import numpy as np
 # indexes
 X,Y,Z = 0,1,2
 
+# shifts for derivatives
+F = -1 # shift forwards
+B = +1 # shift backwards
+
 def helmholtz_decomposition(F: np.ndarray,
                             x: np.ndarray,
                             n_workers: int = 1):
     """
-    Author: James Beattie
+    Author: James Beattie (assumes periodic boundary conditions)
     """
     # F is a 4D array, with the last dimension being 3 (for the x, y, z components of the vector field)
     shape = F.shape[:-1]
@@ -64,14 +68,14 @@ def helmholtz_decomposition(F: np.ndarray,
     
     return F_irrot, F_solen
 
-def curl(field):
+
+def vector_curl(field):
     """
-    Compute the vector curl
+    Compute the vector curl (assumes periodic boundary conditions) 
+    using second order finite differences
     
     Author: James Beattie
     """
-    # indexes
-    x,y,z = 0,1,2
     
     # differentials
     two_dx = 2./field[0].shape[0]
@@ -79,22 +83,43 @@ def curl(field):
     two_dz = 2./field[2].shape[0]
     
     # x component of curl
-    dFz_dy = (np.roll(field[z],-1,axis=y) - np.roll(field[z],1,axis=y))/two_dy
-    dFy_dz = (np.roll(field[y],-1,axis=z) - np.roll(field[y],1,axis=z))/two_dz
+    dFz_dy = (np.roll(field[Z],F,axis=Y) - np.roll(field[Z],B,axis=Y))/two_dy
+    dFy_dz = (np.roll(field[Y],F,axis=Z) - np.roll(field[Y],B,axis=Z))/two_dz
     
     # y component of curl
-    dFx_dz = (np.roll(field[x],-1,axis=z) - np.roll(field[x],1,axis=z))/two_dz
-    dFz_dx = (np.roll(field[z],-1,axis=x) - np.roll(field[z],1,axis=x))/two_dx
+    dFx_dz = (np.roll(field[X],F,axis=Z) - np.roll(field[X],B,axis=Z))/two_dz
+    dFz_dx = (np.roll(field[Z],F,axis=X) - np.roll(field[Z],B,axis=X))/two_dx
     
     # z component of curl
-    dFy_dx = (np.roll(field[y],-1,axis=x) - np.roll(field[y],1,axis=x))/two_dx
-    dFx_dy = (np.roll(field[x],-1,axis=y) - np.roll(field[x],1,axis=y))/two_dy
+    dFy_dx = (np.roll(field[Y],F,axis=X) - np.roll(field[Y],B,axis=X))/two_dx
+    dFx_dy = (np.roll(field[X],F,axis=Y) - np.roll(field[X],B,axis=Y))/two_dy
     
     return np.array([dFz_dy - dFy_dz,
                      dFx_dz - dFz_dx,
                      dFy_dx - dFx_dy])
     
-def vectorCrossProduct(vector1, vector2):
+    
+def scalar_laplacian(field):
+    """
+    Compute the scalar laplacian (assumes periodic boundary conditions)
+    using second order finite differences
+    
+    Author: James Beattie
+    """
+        
+    # differentials
+    dx = 1./field[0].shape[0]
+    dy = 1./field[1].shape[0]
+    dz = 1./field[2].shape[0]
+    
+    d2Fx_dx2 = (np.roll(field[X],F,axis=X) - 2*field[X] + np.roll(field[X],B,axis=X))/dx**2
+    d2Fy_dy2 = (np.roll(field[Y],F,axis=Y) - 2*field[Y] + np.roll(field[Y],B,axis=Y))/dy**2
+    d2Fz_dz2 = (np.roll(field[Z],F,axis=Z) - 2*field[Z] + np.roll(field[Z],B,axis=Z))/dz**2
+    
+    return d2Fx_dx2 + d2Fy_dy2 + d2Fz_dz2
+        
+        
+def vector_cross_product(vector1, vector2):
     """
     Compute the vector cross product of two vectors.
     
@@ -102,6 +127,7 @@ def vectorCrossProduct(vector1, vector2):
     
     Author: Neco Kriel
     """
+    
     vector3 = np.array([
     vector1[Y] * vector2[Z] - vector1[Z] * vector2[Y],
     vector1[Z] * vector2[X] - vector1[X] * vector2[Y],
@@ -109,7 +135,8 @@ def vectorCrossProduct(vector1, vector2):
     ])
     return vector3    
 
-def vectorDotProduct(vector1, vector2):
+
+def vector_dot_product(vector1, vector2):
     """
     Compute the vector dot product of two vectors.
     
@@ -123,7 +150,8 @@ def vectorDotProduct(vector1, vector2):
     ], axis=0)
     return scalar
 
-def fieldMagnitude(vector_field):
+
+def field_magnitude(vector_field):
     """
     Compute the vector magnitude of a vector.
     
@@ -131,6 +159,7 @@ def fieldMagnitude(vector_field):
     """
     vector_field = np.array(vector_field)
     return np.sqrt(np.sum(vector_field**2, axis=0))
+
 
 def gradient_2ocd(field, cell_width, gradient_dir):
     """
@@ -146,7 +175,8 @@ def gradient_2ocd(field, cell_width, gradient_dir):
         np.roll(field, F, axis=gradient_dir) - np.roll(field, B, axis=gradient_dir)
     ) / (2*cell_width)
 
-def fieldRMS(scalar_field):
+
+def field_RMS(scalar_field):
     """
     Compute the root-mean-squared of a field.
     
@@ -154,7 +184,8 @@ def fieldRMS(scalar_field):
     """
     return np.sqrt(np.mean(scalar_field**2))
 
-def fieldGradient(scalar_field):
+
+def field_gradient(scalar_field):
     """
     Compute the gradient of a scalar field.
     
@@ -169,7 +200,8 @@ def fieldGradient(scalar_field):
     ]
     return np.array(field_gradient)
 
-def computeTNBBasis(vector_field):
+
+def compute_TNB_basis(vector_field):
     """
     Compute the Fressnet frame of a vector field (TNB basis).
     
@@ -177,12 +209,12 @@ def computeTNBBasis(vector_field):
     """
     ## format: (component, x, y, z)
     vector_field = np.array(vector_field)
-    field_magn = fieldMagnitude(vector_field)
+    field_magn = field_magnitude(vector_field)
     ## ---- COMPUTE TANGENT BASIS
     t_basis = vector_field / field_magn
     ## df_j/dx_i: (component-j, gradient-direction-i, x, y, z)
     gradient_tensor = np.array([
-        fieldGradient(field_component)
+        field_gradient(field_component)
         for field_component in vector_field
     ])
     ## ---- COMPUTE NORMAL BASIS
@@ -193,12 +225,12 @@ def computeTNBBasis(vector_field):
     ## (f_i df_j/dx_i) / (f_k f_k) - (f_i f_j f_m df_m/dx_i) / (f_k f_k)^2
     n_basis = n_basis_term1 / field_magn**2 - n_basis_term2 / field_magn**4
     ## field curvature
-    kappa = fieldMagnitude(n_basis)
+    kappa = field_magnitude(n_basis)
     ## normal basis
     n_basis /= kappa
     ## ---- COMPUTE BINORMAL BASIS
     ## orthogonal to both t- and b-basis
-    b_basis = vectorCrossProduct(t_basis, n_basis)
+    b_basis = vector_cross_product(t_basis, n_basis)
     return t_basis, n_basis, b_basis, kappa
 
 
