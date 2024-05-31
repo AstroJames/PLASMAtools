49a7bfe9600bd58d02af5f19dcca90cb71375e67
AstroJames <james.beattie@princeton.edu>
2024-05-31 10:31:25 -0400

    Cleaned up derivative stencil class.

--- aux_funcs/derivatives.py
+++ aux_funcs/derivatives.py
@@ -20,6 +20,16 @@ class derivative:
     def __init__(self,
                  order        : int = 2,
                  L            : float = 1.0) -> None:
+        """
+        Initialize the derivative stencil class.
+
+        Author: James Beattie
+
+        Args:
+            order (int, optional):  the order of the stencil. Defaults to 2.
+            L (float, optional):    the size of the domain. Defaults to 1.0.
+        """
+        
         self.order          = order
         self.L              = L
         
@@ -33,37 +43,38 @@ class derivative:
         Author: James Beattie & Neco Kriel
         
         Args:
-            args (_type_): 
+            scalar_field (np.ndarray): the scalar field to compute the gradient of.
+            gradient_dir (int):        the direction of the gradient.
 
         Returns:
-            _type_: _description_
+            np.ndarray: the gradient of the scalar field in the specified direction.
         
         """
         # two point stencil
         if self.order == 2:
             try:
-                two_dr = 2. * self.L /scalar_field.shape[gradient_dir]
+                dr = 2. * self.L /scalar_field.shape[gradient_dir]
             except:
                 return np.zeros_like(scalar_field)
             return ( np.roll(scalar_field, F, axis=gradient_dir) \
-                   - np.roll(scalar_field, B, axis=gradient_dir) ) / two_dr
+                   - np.roll(scalar_field, B, axis=gradient_dir) ) / dr
 
         # four point stencil
         elif self.order == 4:
             try:
-                twelve_dr = 12. * self.L /scalar_field.shape[self.gradient_dir]
+                dr = 12. * self.L /scalar_field.shape[self.gradient_dir]
             except:
                 return np.zeros_like(scalar_field)
             # df/dr = (-f(r+2dr) + 8f(r+dr) - 8f(r-dr) + f(r-2dr))/12dr
             return ( - np.roll(scalar_field,2*F,axis=gradient_dir)  \
                      + 8*np.roll(scalar_field,F,axis=gradient_dir)  \
                      - 8*np.roll(scalar_field,B,axis=gradient_dir)  \
-                     + np.roll(scalar_field,2*B,axis=gradient_dir)) / twelve_dr
+                     + np.roll(scalar_field,2*B,axis=gradient_dir)) / dr
 
         # six point stencil
         elif self.order == 6:
             try:
-                twelve_dr = 60. * self.L / scalar_field.shape[gradient_dir]
+                dr = 60. * self.L / scalar_field.shape[gradient_dir]
             except:
                 return np.zeros_like(scalar_field)
             
@@ -73,4 +84,4 @@ class derivative:
                     - 45*np.roll(scalar_field,B,axis=gradient_dir)  \
                     + 45*np.roll(scalar_field,F,axis=gradient_dir)  \
                     - 9*np.roll(scalar_field,2*F,axis=gradient_dir) \
-                    + np.roll(scalar_field,3*F,axis=gradient_dir))  / twelve_dr
+                    + np.roll(scalar_field,3*F,axis=gradient_dir))  / dr
